7eb8fb0b7e5f3ec3a4723212db3ba533c2267800
hhxkrgzn <hhxkrgzn@com.cn>
2023-10-30 11:04:40 +0800

    update

--- lib/api/__init__.py
+++ lib/api/__init__.py
@@ -6,7 +6,8 @@ GUILD_ID = getenv("GUILD_ID")
 CHANNEL_ID = getenv("CHANNEL_ID")
 USER_TOKEN = getenv("USER_TOKEN")
 CALLBACK_URL = getenv("CALLBACK_URL")
-SESSION_ID = getenv("SESSION_ID")
+SESSION_ID = getenv("SESSION_ID") \
+    or "cb06f61453064c0983f2adae2a88c223"
 
 if not all([GUILD_ID, CHANNEL_ID, USER_TOKEN]):
     raise MissRequiredVariableError("Missing required environment variable: [GUILD_ID, CHANNEL_ID, USER_TOKEN]")
